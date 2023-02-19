6415ded13d13f7afc0b2b3dfa214180f8eda22aa
Ankur <ankur160128@gmail.com>
2023-02-19 18:35:11 +0530

    stories update

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -3,6 +3,12 @@ import Starbucks from '../assets/images/starbucks.png';
 import BarbequeNation from '../assets/images/barbeque_nation.jpeg';
 import Hooters from '../assets/images/hooters.png';
 import LouisBurger from '../assets/images/louis_burger.png';
+import KFC from '../assets/images/kfc.jpeg';
+import Subway from '../assets/images/subway.jpeg';
+import Chaayos from '../assets/images/chaayos.jpeg';
+import BeerHouseCafe from '../assets/images/beer_house_cafe.png';
+import ChaiPoint from '../assets/images/chai_point.jpeg';
+import Nandos from '../assets/images/nandos.jpeg';
 
 export const stories: RestaurantStory[] = [
   {
@@ -22,7 +28,37 @@ export const stories: RestaurantStory[] = [
   },
   {
     id: 4,
-    name: 'Louis',
+    name: 'Louis Burger',
     logo: LouisBurger
+  },
+  {
+    id: 5,
+    name: 'KFC',
+    logo: KFC
+  },
+  {
+    id: 6,
+    name: 'Chaayos',
+    logo: Chaayos
+  },
+  {
+    id: 7,
+    name: 'The Beer House Cafe',
+    logo: BeerHouseCafe
+  },
+  {
+    id: 8,
+    name: 'Chai Point',
+    logo: ChaiPoint
+  },
+  {
+    id: 9,
+    name: "Nando's",
+    logo: Nandos
+  },
+  {
+    id: 10,
+    name: 'Subway',
+    logo: Subway
   },
 ]
--- src/molecules/StoriesTray/StoriesTray.tsx
+++ src/molecules/StoriesTray/StoriesTray.tsx
@@ -1,21 +1,18 @@
 import Image from "next/image";
-import Starbucks from '../../assets/images/starbucks.png';
-import BarbequeNation from '../../assets/images/barbeque_nation.jpeg';
-import Hooters from '../../assets/images/hooters.png';
-import LouisBurger from '../../assets/images/louis_burger.png';
 import { stories } from "@/constants/data";
+import clsx from "clsx";
 
 export const StoriesTray = () => {
   return (
-    <div className="flex items-center">
-      {stories.map((story, index) => (
-        <div key={index} className={"flex flex-col items-center mr-5"}>
-          <div className="p-0.5 border border-dashed border-white rounded-full">
-            <Image src={story.logo} alt="" className="w-12 h-12 rounded-full" />
+    <div className="w-full flex items-center overflow-x-auto no-scrollbar">
+      {stories.map((story, index) =>
+        <div key={index} className={clsx(index === stories.length - 1 ? '' : 'mr-3', "flex flex-col items-center w-14")}>
+          <div className="w-12 h-12 flex items-center justify-center p-[0.2rem] border border-dashed border-white rounded-full">
+            <Image src={story.logo} alt="" className="w-10 h-10 rounded-full" />
           </div>
-          <div className="text-xs font-normal text-gray-200 mt-1">{story.name}</div>
+          <div className="text-xs font-normal text-gray-200 mt-1 w-full truncate text-center">{story.name}</div>
         </div>
-      ))}
+      )}
     </div>
   )
 }
